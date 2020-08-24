7a8aca950cc33b0e19ac14b680eeaa0d9c1700ab
Larissa Ellen Gomes Marques <larissa.marques@.stitdata.com>
2020-08-24 13:46:27 -0300

    up

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -3,4 +3,13 @@ angular.module('ifsp').controller('ContatosController', function ($scope) {
   $scope.incrementa = function () {
     $scope.total++;
   };
+
+  $scope.contatos = [
+    { _id: 1, nome: 'Larissa Marques', email: 'larissa.marques@ifsp.edu.br' },
+    { _id: 2, nome: 'Daniel Samarone', email: 'daniel.samarone@ifsp.edu.br' },
+    { _id: 3, nome: 'Gabriela Mota', email: 'gabriela.mota@ifsp.edu.br' },
+    { _id: 4, nome: 'Rodrigo Inoue', email: 'rodrigo.inoue@ifsp.edu.br' },
+  ];
+
+  $scope.filter = '';
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
-angular.module('ifsp', []);
+angular.module('ifsp', ['ngRoute']);
+
+angular.module('ifsp', ['ngRoute']).config(function ($routeProvider) {
+  $routeProvider.when('/contatos', {
+    templateUrl: './partials/contatos.html',
+    controller: 'ContatosController',
+  });
+
+  $routeProvider.when('/contato/:contatoId', {
+    templateUrl: 'partials/contato.html',
+    controller: 'ContatoController',
+  });
+
+  $routeProvider.otherwise({ redirectTo: '/contatos' });
+});
